fcbc1cf724c9fb8f28deebf1c39fd2c741488b4d
inu255 <artmvhdv@mail.ru>
2022-11-28 00:38:52 +0700

    grid footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,13 @@
 import { FooterProps } from "./Footer.props";
-import styles from "./Header.module.css";
+import styles from "./Footer.module.css";
 import cn from "classnames";
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <div {...props} className={cn(className, styles.container)}>
+      <div>OwiTop © 2020 - 2022 Все права защищены</div>
+      <div>Пользовательское соглашение</div>
+      <div>Политика конфиденциальности</div>
+    </div>
+  );
 };
