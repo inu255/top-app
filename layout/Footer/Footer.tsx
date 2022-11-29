698a1b09d8deda1e4d79ab16417e3c900c3d9ebe
inu255 <artmvhdv@mail.ru>
2022-11-30 00:33:07 +0700

    footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,13 +1,18 @@
 import { FooterProps } from "./Footer.props";
 import styles from "./Footer.module.css";
 import cn from "classnames";
+import { format } from "date-fns";
 
 export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
   return (
-    <div {...props} className={cn(className, styles.container)}>
-      <div>OwiTop © 2020 - 2022 Все права защищены</div>
-      <div>Пользовательское соглашение</div>
-      <div>Политика конфиденциальности</div>
+    <div {...props} className={cn(className, styles.footer)}>
+      <div>OwiTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
     </div>
   );
 };
